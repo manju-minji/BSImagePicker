48fa0d53779ed4271f57c2ac29e848475b4d6680
manju-minji <0923alswl@gmail.com>
2021-08-12 21:48:14 +0900

    change cancel function public

--- Sources/Controller/ImagePickerController+ButtonActions.swift
+++ Sources/Controller/ImagePickerController+ButtonActions.swift
@@ -23,7 +23,7 @@
 import UIKit
 
 extension ImagePickerController {
-    @objc func albumsButtonPressed(_ sender: UIButton) {
+    @objc public func albumsButtonPressed(_ sender: UIButton) {
         albumsViewController.albums = albums
         
         // Setup presentation controller
@@ -34,7 +34,7 @@ extension ImagePickerController {
         present(albumsViewController, animated: true)
     }
 
-    @objc func cancelButtonPressed(_ sender: UIBarButtonItem) {
+    @objc public func cancelButtonPressed(_ sender: UIBarButtonItem) {
         imagePickerDelegate?.imagePicker(self, didCancelWithAssets: assetStore.assets)
         
         if settings.dismiss.enabled {
